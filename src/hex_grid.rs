b2c329c550003d63f4bd4d2b18fa9d0a3c81c0de
Morphylus <martindefuns@kns.ch>
2025-01-15 20:52:53 +0100

    fix: hover issue this time for real

--- src/hex_grid.rs
+++ src/hex_grid.rs
@@ -6,7 +6,17 @@ pub struct HexGridPlugin;
 
 impl Plugin for HexGridPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, sync_transform_with_hex_coords);
+        app.insert_resource(HoveredTile::default())
+            .add_systems(Startup, setup_hover_indicator)
+            .add_systems(
+                Update,
+                (
+                    check_hex_hover_position,
+                    update_hover_indicator,
+                    sync_transform_with_hex_coords,
+                )
+                    .chain(),
+            );
     }
 }
 
@@ -39,5 +49,103 @@ fn sync_transform_with_hex_coords(
         let screen_coords = hex_coord.to_screen_coords(board_settings.tile_size);
         transform.translation.x = screen_coords.x;
         transform.translation.y = screen_coords.y;
+    }
+}
+fn setup_hover_indicator(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    board_settings: Res<BoardSettings>,
+) {
+    commands.spawn((
+        HoverIndicator,
+        HexCoord { r: 0, q: 0, s: 0 },
+        Mesh2d(meshes.add(RegularPolygon::new(board_settings.tile_size, 6))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(1.0, 0.0, 0.0)))),
+        Transform::from_xyz(0.0, 0.0, 1.0),
+        Visibility::Hidden,
+    ));
+}
+
+#[derive(Resource)]
+struct HoveredTile {
+    position: Option<HexCoord>,
+}
+
+impl Default for HoveredTile {
+    fn default() -> Self {
+        HoveredTile { position: None }
+    }
+}
+
+#[derive(Component)]
+struct HoverIndicator;
+
+fn update_hover_indicator(
+    hovered_tile: Res<HoveredTile>,
+    mut query: Query<(&mut HexCoord, &mut Visibility), With<HoverIndicator>>,
+) {
+    let (mut entity_hex_coord, mut visibility) = query.single_mut();
+
+    if let Some(pos) = hovered_tile.position {
+        *entity_hex_coord = pos
+    }
+
+    if hovered_tile.position.is_some() {
+        *visibility = Visibility::Visible
+    } else {
+        *visibility = Visibility::Hidden
+    }
+}
+
+fn check_hex_hover_position(
+    window: Query<&Window>,
+    mut hovered_tile: ResMut<HoveredTile>,
+    board_settings: Res<BoardSettings>,
+) {
+    let window = window.single();
+    let window_width = window.width();
+    let window_height = window.height();
+    let tile_size = board_settings.tile_size;
+    let board_size = board_settings.board_radius;
+
+    if let Some(position) = window.cursor_position() {
+        let centered_x = position.x - 0.5 * window_width;
+        let centered_y = position.y - 0.5 * window_height;
+        let frac_q = (3_f32.sqrt() / 3.0 * centered_x + -1.0 / 3.0 * centered_y) / tile_size;
+        let frac_r = (2.0 / 3.0) * centered_y / tile_size;
+        let frac_s = -frac_q - frac_r;
+
+        // Rounding to nearest cube coordinate
+        let mut q = frac_q.round();
+        let mut r = frac_r.round();
+        let mut s = frac_s.round();
+
+        let q_diff = (q - frac_q).abs();
+        let r_diff = (r - frac_r).abs();
+        let s_diff = (s - frac_s).abs();
+
+        if q_diff > r_diff && q_diff > s_diff {
+            q = -r - s;
+        } else if r_diff > s_diff {
+            r = -q - s;
+        } else {
+            s = -q - r;
+        }
+
+        let hex_pos = HexCoord {
+            q: (q as i32),
+            r: (r as i32),
+            s: (s as i32),
+        };
+
+        if q.abs() as i32 <= board_size
+            && r.abs() as i32 <= board_size
+            && s.abs() as i32 <= board_size
+        {
+            hovered_tile.position = Some(hex_pos)
+        } else {
+            hovered_tile.position = None;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,100 +10,12 @@ fn main() {
     app.add_plugins((DefaultPlugins, Wireframe2dPlugin, HexGridPlugin))
         .insert_resource(Board::default())
         .insert_resource(BoardSettings::default())
-        .insert_resource(HoveredTile::default())
-        .add_systems(
-            Startup,
-            (setup, setup_board, spawn_settler, setup_hover_indicator),
-        )
-        .add_systems(Update, (check_hex_hover_position, update_hover_indicator))
+        .add_systems(Startup, (setup, setup_board, spawn_settler))
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
-}
-
-fn check_hex_hover_position(
-    window: Query<&Window>,
-    mut hovered_tile: ResMut<HoveredTile>,
-    board_settings: Res<BoardSettings>,
-) {
-    let window = window.single();
-    let window_width = window.width();
-    let window_height = window.height();
-    let tile_size = board_settings.tile_size;
-    let board_size = board_settings.board_radius;
-
-    if let Some(position) = window.cursor_position() {
-        let centered_x = position.x - 0.5 * window_width;
-        let centered_y = position.y - 0.5 * window_height;
-        let frac_q = (3_f32.sqrt() / 3.0 * centered_x + -1.0 / 3.0 * centered_y) / tile_size;
-        let frac_r = (2.0 / 3.0) * centered_y / tile_size;
-        let frac_s = -frac_q - frac_r;
-
-        // Rounding to nearest cube coordinate
-        let mut q = frac_q.round();
-        let mut r = frac_r.round();
-        let mut s = frac_s.round();
-
-        let q_diff = (q - frac_q).abs();
-        let r_diff = (r - frac_r).abs();
-        let s_diff = (s - frac_s).abs();
-
-        if q_diff > r_diff && q_diff > s_diff {
-            q = -r - s;
-        } else if r_diff > s_diff {
-            r = -q - s;
-        } else {
-            s = -q - r;
-        }
-
-        let hex_pos = HexCoord {
-            q: (q as i32),
-            r: (r as i32),
-            s: (s as i32),
-        };
-
-        if q.abs() as i32 <= board_size
-            && r.abs() as i32 <= board_size
-            && s.abs() as i32 <= board_size
-        {
-            hovered_tile.position = Some(hex_pos)
-        } else {
-            hovered_tile.position = None;
-        }
-    }
-}
-
-fn update_hover_indicator(
-    hovered_tile: Res<HoveredTile>,
-    mut query: Query<(&mut HexCoord, &mut Visibility), With<HoverIndicator>>,
-) {
-    let (mut entity_hex_coord, mut visibility) = query.single_mut();
-
-    match hovered_tile.position {
-        Some(pointer_hex_coord) => {
-            *entity_hex_coord = pointer_hex_coord;
-            *visibility = Visibility::Visible
-        }
-        None => *visibility = Visibility::Hidden,
-    }
-}
-
-fn setup_hover_indicator(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    board_settings: Res<BoardSettings>,
-) {
-    commands.spawn((
-        HoverIndicator,
-        HexCoord { r: 0, q: 0, s: 0 },
-        Mesh2d(meshes.add(RegularPolygon::new(board_settings.tile_size, 6))),
-        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(1.0, 0.0, 0.0)))),
-        Transform::from_xyz(0.0, 0.0, 1.0),
-        Visibility::Hidden,
-    ));
 }
 
 fn spawn_settler(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -118,20 +30,6 @@ fn spawn_settler(mut commands: Commands, asset_server: Res<AssetServer>) {
         spawn_coords,
         Transform::from_xyz(0.0, 0.0, 2.0),
     ));
-}
-
-#[derive(Component)]
-struct HoverIndicator;
-
-#[derive(Resource)]
-struct HoveredTile {
-    position: Option<HexCoord>,
-}
-
-impl Default for HoveredTile {
-    fn default() -> Self {
-        HoveredTile { position: None }
-    }
 }
 
 #[derive(Component)]
